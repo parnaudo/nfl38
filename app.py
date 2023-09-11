7a8111a6d1c5bfbd5faf9b3b7555bc819e32f0cd
Paul Arnaudo <parnaudo@gmail.com>
2023-09-10 21:12:37 -0700

    trying to add in the winners notification

--- app.py
+++ app.py
@@ -27,7 +27,13 @@ headers = {
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 value = 1
 
-
+sports_dict = {
+    "New York Jets": 'Paul',
+    "Chicago Bears": 'Phil',
+    "New England Patriots": 'Jovany',
+    "Indianapolis Colts": 'Scott',
+    "Detroit Lions": "Erik"
+}
 
 # response = requests.post('http://ntfy.sh/nfl38club', headers=headers, data=data)
 home_team_display_name = nfl_json["events"][0]['competitions'][0]['competitors'][0]['team']['displayName']
@@ -35,24 +41,33 @@ home_team_display_name = nfl_json["events"][0]['competitions'][0]['competitors']
 away_team_display_name = nfl_json["events"][0]['competitions'][0]['competitors'][1]['team']['displayName']
 for events in nfl_json["events"]:
     matchup = events['name']
+    timeleft = events['status']['type']['detail']
     for competitors in events['competitions'][0]['competitors']:
         score = competitors['score']
         team_display_name = competitors['team']['displayName']
         # pprint.pprint(competitors)
-        key = f"{team_display_name}{events['shortName']}{nfl_json['season']['year']}".replace(" ","")
-        
-        if r.exists(key) == False:
+        progress_key = f"{team_display_name}{events['shortName']}{nfl_json['season']['year']}".replace(" ","")
+        winning_key = f"{events['status']['type']['detail']}{team_display_name}{events['shortName']}{nfl_json['season']['year']}".replace(" ","")
+        print("WINNINGKEY: ",winning_key)
+
+        if timeleft == 'Final' and int(score) == target_score:
+            if r.exists(winning_key) == False:
+                r.set(winning_key,value)
+                winning_message = f"The {team_display_name} finished the {matchup} game with {target_score} points, congrats to {sports_dict[team_display_name]}"
+                print(winning_message)
+                response = requests.post('http://ntfy.sh/nfl38club', headers=headers, data=winning_message)
+        elif r.exists(progress_key) == False:
             if int(score) == int(target_score_minus_fg):
-                message = f"The {team_display_name} are a field goal away from the magic {target_score} with a score of {score} in the matchup: {matchup} "
+                message = f"The {team_display_name} are a field goal away from the magic {target_score} with a score of {score} in the matchup: {matchup} with the clock at {timeleft} "
                 response = requests.post('http://ntfy.sh/nfl38club', headers=headers, data=message)
-                print(key)
-                r.set(key,value)
+                print("PROGRESSKEY: ",progress_key)
+                r.set(progress_key,value)
                 print(message)
-            if int(score) == int(target_score_minus_td):
-                message = f"The {team_display_name} are a touchdown away from the magic {target_score} with a score of {score} in the matchup: {matchup} "
+            elif int(score) == int(target_score_minus_td):
+                message = f"The {team_display_name} are a touchdown away from the magic {target_score} with a score of {score} in the matchup: {matchup} with the clock at {timeleft} "
                 response = requests.post('http://ntfy.sh/nfl38club', headers=headers, data=message)
-                print(key)
-                r.set(key,value)
+                print(progress_key)
+                r.set(progress_key,value)
                 print(message)
             # send_twilio_sms(phone_numbers,home_team_display_name,home_team_score,messaging_service_sid)
         # if away_team_score == target_score_minus_fg or away_team_score == target_score_minus_td:
